2ebf817f1e38a9cc09d5c972ca4dd6e488e80808
Gabriel Bastos <gabriel.bsandim@gmail.com>
2025-02-12 17:20:51 -0300

    refactor: added some configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,18 @@
 import type { Config } from '@jest/types'
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+import { compilerOptions } from './tsconfig.json'
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/tests/(.*)$': '<rootDir>/tests/$1',
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
+    },
   },
   setupFiles: ['<rootDir>/tests/setup.ts'],
 }
